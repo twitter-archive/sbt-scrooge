17933870a80721e184317dee1eb5fa3005c8decc
Jeremy Cloud <jeremycloud@twitter.com>
2011-10-07 09:56:00 -0400

    latest scrooge

--- src/main/scala/com/twitter/sbt/CompileScrooge.scala
+++ src/main/scala/com/twitter/sbt/CompileScrooge.scala
@@ -13,7 +13,7 @@ trait CompileThriftScroogeMixin extends DefaultProject {
   /**
    * Override this to use a different version of scrooge for code generation.
    */
-  def scroogeVersion = "1.1.7"
+  def scroogeVersion = "2.0.1"
 
   /**
    * Override these to turn on/off generating ostrich or finagle bindings.
