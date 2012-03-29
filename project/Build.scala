b3c181f9eb589baac85cb4568d3468ae55759ae0
Robey Pointer <robey@twitter.com>
2012-03-29 11:54:16 -0700

    'release commit for com.twitter:sbt11-scrooge:1.0.1-SNAPSHOT'

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,7 @@ object SbtScroogePlugin extends Build {
   ).settings(
     name := "sbt11-scrooge",
     organization := "com.twitter",
-    version := "1.0.0",
+    version := "1.0.1-SNAPSHOT",
     sbtPlugin := true,
 
     SubversionPublisher.subversionRepository := Some("https://svn.twitter.biz/maven-public")
