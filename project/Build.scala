ef4416c2ac39a56fe54eca48216fb366a7860b55
Robey Pointer <robey@twitter.com>
2012-03-29 11:50:36 -0700

    push up to 1.0.0

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,7 @@ object SbtScroogePlugin extends Build {
   ).settings(
     name := "sbt11-scrooge",
     organization := "com.twitter",
-    version := "0.99.1-SNAPSHOT",
+    version := "1.0.0-SNAPSHOT",
     sbtPlugin := true,
 
     SubversionPublisher.subversionRepository := Some("https://svn.twitter.biz/maven-public")
